b7b7952bfcbc9e369d57438ada64f8579e9bbc62
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-06-04 00:21:46 +0100

    png error

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import to_doback from "../assets/to-do-back.svg";
-import logo from "../assets/logo.png";
+// import logo from "../assets/logo.png";
 
 const HomePage = () => {
   return (
     <Wrapper>
-      <div className='logo'>
+      {/* <div className='logo'>
         <img src={logo} alt='' />
-      </div>
+      </div> */}
       <h1 className='title-home'>Welcome to Your To-Do App</h1>
       <p className='text'>
         Manage your tasks efficiently with our easy to use to-do app.
